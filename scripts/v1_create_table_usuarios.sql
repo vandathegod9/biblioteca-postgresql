88077288eaedef6cc7908bb04e2b9542762460d9
twz <guesso78k@gmail.com>
2026-09-08 18:22:13 -0300

    feat: adiciona criação da tabela usuarios

---
+++ scripts/v1_create_table_usuarios.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS usuarios (
+    id_usuario SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    telefone VARCHAR(20),
+    data_cadastro DATE NOT NULL DEFAULT CURRENT_DATE
+);
