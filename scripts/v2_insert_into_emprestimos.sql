5d1d00b140805a03028384fb1efe78f62f0f92ba
twz <guesso78k@gmail.com>
2026-09-08 18:29:58 -0300

    feat: adiciona dados de exemplo dos emprestimos

---
+++ scripts/v2_insert_into_emprestimos.sql
@@ -0,0 +1,7 @@
+INSERT INTO emprestimos
+    (id_usuario, data_emprestimo, data_devolucao, status)
+VALUES
+    (1, '2026-09-01', NULL, 'ABERTO'),
+    (2, '2026-09-02', '2026-09-07', 'DEVOLVIDO'),
+    (3, '2026-09-03', NULL, 'ABERTO'),
+    (4, '2026-09-04', '2026-09-08', 'DEVOLVIDO');
